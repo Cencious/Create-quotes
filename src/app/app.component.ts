fa480835592282dc1df420173fbddf91f1a03abe
Cencious <user.email>
2022-04-10 19:47:40 +0300

    add another form component for quote 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,14 +5,22 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent 
-{
+export class AppComponent {
   allQuotes = [
-  {
-  quote: "quote 1",
-  author: "person1",
-  owner: "person2",
-  upVote: 0,
-  downVote: 0
+    {
+      quote: "quote 1",
+      author: "person1",
+      owner: "person2",
+      upVote: 0,
+      downVote: 0
+    },
+    {
+      quote: "quote 2",
+      author: "name",
+      owner: "name",
+      upVote: 0,
+      downVote: 0
+    }
+  ]
 
-  }]
+  
