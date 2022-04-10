ce61c55dbba7f67b1a8da0bda8687838c6669117
Cencious <user.email>
2022-04-10 23:40:04 +0300

    add binding properties

---
+++ src/app/components/quote/quote.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent {
+  @Input() quote: Quote;
+  @Output() upvote = new EventEmitter<Quote>();
+  @Output() downvote = new EventEmitter<Quote>();
+}
