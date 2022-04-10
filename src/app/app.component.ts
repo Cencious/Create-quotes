bc6cf5e69b33d588c692f3c3a2efd87ffcd00a36
Cencious <user.email>
2022-04-10 19:44:25 +0300

    add initial upvote and downvote 0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,3 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent 
+{
+  allQuotes = [
+  {
+  quote: "quote 1",
+  author: "person1",
+  owner: "person2",
+  upVote: 0,
+  downVote: 0
+
+  }]
