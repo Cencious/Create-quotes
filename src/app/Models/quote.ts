5b51ca72fb9dcf49eede0cdeb05e77cf98c6bbc3
Cencious <user.email>
2022-04-11 07:55:00 +0300

    add more information

--- src/app/Models/quote.ts
+++ src/app/Models/quote.ts
@@ -1,3 +1,7 @@
 export class Quote {
-  
+    showDetails: boolean;
+    showInfo:any
+    constructor(public id:number, public author:string, public owner:string, public quote:string, public datePosted: Date, public likes:number, public dislikes:number) {
+        this.showDetails=false;
+    }
 }
